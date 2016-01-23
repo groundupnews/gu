27bea6712adb969ac95878a307094928499e8040
Nathan Geffen <nathangeffen@gmail.com>
2016-01-23 09:53:43 +0200

    Intro style for ckeditor.

--- newsroom/static/newsroom/js/ck_inline_config.js
+++ newsroom/static/newsroom/js/ck_inline_config.js
@@ -7,7 +7,7 @@ CKEDITOR.stylesSet.add( 'gu_styles', [
       attributes: { 'class': 'very-large' } },
     { name: 'Image Caption', element: 'figcaption',
       attributes: { 'class': 'caption' } },
-    { name: 'Paragraph', element: 'p',
+    { name: 'Introduction', element: 'p',
       attributes: { 'class': 'intro' } },
     { name: 'Pullout Quote' , element: 'aside',
       attributes: { 'class': 'pquote' } }
