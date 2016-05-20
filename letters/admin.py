3e2c89a367f34f1381f90d81c6f4f5c0f3571c20
Nathan Geffen <nathangeffen@gmail.com>
2016-05-20 14:47:05 +0200

    Improved letter processing.

--- letters/admin.py
+++ letters/admin.py
@@ -3,12 +3,37 @@ from django.contrib import admin
 from . import models
 
 
+class ProcessedListFilter(admin.SimpleListFilter):
+    title = 'processed'
+    parameter_name = 'processed'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('published', 'published'),
+            ('rejected', 'rejected'),
+            ('processed', 'processed'),
+            ('unprocessed', 'unprocessed'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'published':
+            return queryset.published()
+        if self.value() == 'rejected':
+            return queryset.filter(rejected=True)
+        if self.value() == 'processed':
+            return queryset.published() | queryset.filter(rejected=True)
+        if self.value() == 'unprocessed':
+            return queryset.filter(published__isnull=True).\
+                filter(rejected=False)
+
+
 class LetterAdmin(admin.ModelAdmin):
     list_display = ['article', 'byline', 'email', 'title',
-                    'rejected', 'published', 'is_published', 'modified', ]
-    list_filter = ['rejected', ]
+                    'rejected', 'published', 'modified', ]
+    date_hierarchy = 'modified'
+    list_filter = [ProcessedListFilter, ]
     search_fields = ['article__title', 'title', 'byline', 'email', ]
-    ordering = ['article__modified', '-position', ]
+    ordering = ['-modified', '-position', ]
     raw_id_fields = ('article', )
 
 admin.site.register(models.Letter, LetterAdmin)
