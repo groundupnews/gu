19b95ef6e00db850f94a1eab2e7373dc53a5db34
Nathan Geffen <nathangeffen@gmail.com>
2016-08-29 21:01:31 +0200

    Changed atime to mtime.

--- groundup/imageindexer/index_files.sh
+++ groundup/imageindexer/index_files.sh
@@ -5,7 +5,7 @@ IFS=$(echo -en "\n\b")
 
 daysback=-$1
 
-for file in $(find . -atime $daysback -iname "*.jpg" -o -iname "*.png"); do
+for file in $(find . -mtime $daysback -iname "*.jpg" -o -iname "*.png"); do
 	photographer=$(identify -verbose $file | grep -i exif:Artist | awk '{print $2, $3}')
 	echo $file,$photographer,
 	identify -verbose $file | grep -i keyword | awk -v PHOTOGRAPHER"=$photographer" -v FILE="$file" '{print FILE "," PHOTOGRAPHER "," $2}'
