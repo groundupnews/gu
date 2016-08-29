b410cd1b04fe3e9597ab67b755cc56df0eb1e4c9
Nathan Geffen <nathangeffen@gmail.com>
2016-08-29 21:17:47 +0200

    Added modification date.

--- groundup/imageindexer/index_files.sh
+++ groundup/imageindexer/index_files.sh
@@ -6,9 +6,10 @@ IFS=$(echo -en "\n\b")
 daysback=-$1
 
 for file in $(find . -mtime $daysback -iname "*.jpg" -o -iname "*.png"); do
-	photographer=$(identify -verbose $file | grep -i exif:Artist | awk '{print $2, $3}')
-	echo $file,$photographer,
-	identify -verbose $file | grep -i keyword | awk -v PHOTOGRAPHER"=$photographer" -v FILE="$file" '{print FILE "," PHOTOGRAPHER "," $2}'
+    photographer=$(identify -verbose $file | grep -i exif:Artist | awk '{print $2, $3}')
+    modified=$(stat -c %y $file | awk '{print $1}')
+    echo $file,$photographer,$modified
+    identify -verbose $file | grep -i keyword | awk -v PHOTOGRAPHER"=$photographer" -v FILE="$file" -v MODIFIED="$modified" '{print FILE "," PHOTOGRAPHER "," MODIFIED "," $2}'
 done
 
 #IFS=$SAVEIFS
