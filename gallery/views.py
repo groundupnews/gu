7dc753a36c84c408aa2a0eac24d22509ad9de6a9
Nathan Geffen <nathangeffen@gmail.com>
2016-10-12 23:51:27 +0200

    Added querying to Gallery.

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 
@@ -16,13 +17,30 @@ def album_detail(request, pk):
 
 
 def photo_list(request, keyword=None):
-    if keyword is None:
-        photos = models.Photograph.objects.all()
+    search_string = ""
+    list_all = True
+    if keyword:
+        query = Q(keywords__name__in=[keyword])
+        list_all = False
     else:
-        photos = models.Photograph.objects.filter(keywords__name__in=[keyword])
+        query = Q()
+
+    if "search-photos" in request.GET:
+        search_string = request.GET["search-photos"]
+        search_query = Q(suggested_caption__icontains=search_string) | \
+                       Q(photographer__last_name__icontains=search_string) | \
+                       Q(photographer__first_names__icontains=search_string) | \
+                       Q(keywords__name__in=[search_string])
+        query = query & search_query
+        list_all = False
+
+    photos = models.Photograph.objects.filter(query).distinct()
+
     return render(request, "gallery/photo_list.html",
-                  {'photos':photos,
-                   'keyword': keyword})
+                  {'photos': photos,
+                   'keyword': keyword,
+                   'search_string': search_string,
+                   'list_all': list_all})
 
 
 def photo_detail(request, pk):
