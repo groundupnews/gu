6059f1d8b78531e7914e25a04a1e5fd7289e5548
Nathan Geffen <nathangeffen@gmail.com>
2016-11-09 11:00:13 +0200

    Tweaked admin interface for commissions.

--- payment/admin.py
+++ payment/admin.py
@@ -13,19 +13,34 @@ class InvoiceInline(admin.StackedInline):
     classes = ('grp-closed',)
     extra = 0
 
-
-class UnprocessedListFilter(admin.SimpleListFilter):
-    title = 'Approved'
+class ApprovedCommissionListFilter(admin.SimpleListFilter):
+    title = 'Funded vs unfunded'
     parameter_name = 'approved'
 
     def lookups(self, request, model_admin):
         return (
-            ('unapproved', 'unapproved'),
+            ('approved', 'Funded Commissions'),
+            ('unapproved', 'Unfunded Commissions'),
         )
 
     def queryset(self, request, queryset):
         if self.value() == 'unapproved':
             return queryset.filter(fund__isnull = True)
+        if self.value() == 'approved':
+            return queryset.filter(fund__isnull = False)
+
+class InvoiceStatusListFilter(admin.SimpleListFilter):
+    title = 'Invoice status'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return models.INVOICE_STATUS_CHOICES
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(invoice__status = self.value())
+
+
 
 class CommissionAdmin(admin.ModelAdmin):
     list_display = ('invoice', 'article',
@@ -33,7 +48,8 @@ class CommissionAdmin(admin.ModelAdmin):
     list_editable = ('fund', 'commission_due', 'taxable',)
     search_fields = ('invoice__author__first_names',
                      'invoice__author__last_name',)
-    list_filter = ['invoice__author', UnprocessedListFilter,]
+    list_filter = ['invoice__author', ApprovedCommissionListFilter, \
+                   InvoiceStatusListFilter]
     ordering = ['-modified', ]
     raw_id_fields = ('invoice', 'article', )
     autocomplete_lookup_fields = {
