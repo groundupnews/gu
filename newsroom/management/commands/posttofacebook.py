c837f012c3f34c6fc42f4f01f0122f7f8477d84e
Nathan Geffen <nathangeffen@gmail.com>
2018-04-15 21:46:05 +0200

    Fixed letters.

--- newsroom/management/commands/posttofacebook.py
+++ newsroom/management/commands/posttofacebook.py
@@ -1,37 +1,38 @@
 import datetime
-import logging
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.utils import timezone
 from django.contrib.sites.models import Site
-from django.conf import settings
 import sys
 import facebook
-import pytz
 from newsroom.models import Article
-import html
+import traceback
+
 
 def get_api(cfg):
     graph = facebook.GraphAPI(cfg['access_token'])
     return graph
 
+
 def process(days, max_posts):
     cfg = {
-        "page_id"        : settings.FACEBOOK_PAGE_ID,
-        "access_token"        : settings.FACEBOOK_ACCESS_TOKEN,
+        "page_id": settings.FACEBOOK_PAGE_ID,
+        "access_token": settings.FACEBOOK_ACCESS_TOKEN,
     }
     successes = 0
     failures = 0
     try:
         api = get_api(cfg)
-    except:
+    except Exception as e:
+        print("Exception message:", e)
+        traceback.print_exc()
         print("PostToFacebook: Accessing Facebook failed.")
         print("PostToFacebook: Error: ", sys.exc_info()[0])
         return
 
     date_from = timezone.now() - datetime.timedelta(days=days)
     articles = Article.objects.published().filter(published__gte=date_from). \
-               filter(facebook_send_status="scheduled")
+        filter(facebook_send_status="scheduled")
     post_count = 0
     for article in articles:
         dont_send_before = article.published + \
@@ -44,30 +45,8 @@ def process(days, max_posts):
         message = article.facebook_message
         link = "http://" + \
                Site.objects.all()[0].domain + article.get_absolute_url()
-        if article.facebook_image_caption:
-            caption = article.facebook_image_caption
-        else:
-            caption = article.subtitle
-        if article.facebook_description:
-            description = article.facebook_description
-        else:
-            description = article.cached_summary_text_no_html
-        url = "http://" + Site.objects.all()[0].domain
-        if article.facebook_image:
-            picture = article.facebook_image.url
-        elif article.cached_primary_image:
-            picture = article.cached_primary_image
-        else:
-            picture = ""
-            caption = ""
-        if picture and picture[0] == "/":
-            picture = url + picture
         attachment = {
-            # 'name': html.unescape(article.title).replace(u'\xa0',u''),
             'link': link,
-            # 'caption': html.unescape(caption).replace(u'\xa0',u''),
-            # 'description': html.unescape(description).replace(u'\xa0',u''),
-            # 'picture': picture
         }
         try:
             api.put_wall_post(message=message,
@@ -78,14 +57,16 @@ def process(days, max_posts):
             successes = successes + 1
             article.facebook_send_status = "sent"
             article.save()
-        except:
+        except Exception as e:
+            print("Exception message:", e)
+            traceback.print_exc()
             failures = failures + 1
             article.facebook_send_status = "failed"
             print("PostToFacebook: Error: ", sys.exc_info()[0])
             print("PostToFacebook: Failed post: {}".format(article.title))
             article.save()
 
-    return {"successes" : successes, "failures" : failures}
+    return {"successes": successes, "failures": failures}
 
 
 class Command(BaseCommand):
@@ -93,15 +74,17 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('days', type=int,
-                            help="Number of days back in time to look for posts.")
+                            help="Number of days back "
+                            "in time to look for posts.")
         parser.add_argument('maxposts', type=int,
                             help="Maximum number of posts to send.")
 
     def handle(self, *args, **options):
         days = options["days"]
         max_posts = options["maxposts"]
-        print("PostToFacebook: {0}: Processing {1} days with a maximum of {2} posts.". \
+        print("PostToFacebook: {0}: Processing {1} days "
+              "with a maximum of {2} posts.".
               format(str(timezone.now()), days, max_posts))
         success_dict = process(days, max_posts)
-        print("PostToFacebook: Successful: {0}. Failed: {1}".\
+        print("PostToFacebook: Successful: {0}. Failed: {1}".
               format(success_dict["successes"], success_dict["failures"]))
