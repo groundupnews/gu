b2fc9bd1e4c278f407ae470575e6a3b5fb2499fc
gary <gary@impd.co.za>
2018-10-09 20:15:04 +0200

    Disable buttons if no search string is provided

--- templates/advanced_search.js
+++ templates/advanced_search.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {
     var search_input = $('.header__search__input')
+    var search_button = $('#search-button')
     var advanced_search = $('.advanced-search')
+    var advanced_search_input = $('#id_adv_search')
+    var advanced_search_button = $('.adv-filter-submit')
     var header_bar = $('.header')
     var adv_arrow = $('.adv-filter-arrow')
     var adv_form = $('.adv-filter-form')
@@ -13,7 +16,33 @@ $( document ).ready(function() {
             advanced_search.slideUp('slow')
         })
     }
-        
+
+    if (search_input.val() !== undefined && search_input.val().trim() === "") {
+        search_button.prop('disabled', true)
+    }
+
+    search_input.on('keyup paste', function() {
+        var input = $(this)
+        if (input.val().trim() === "") {
+            search_button.prop('disabled', true)
+        } else {
+            search_button.prop('disabled', false)
+        }
+    })
+
+    if (advanced_search_input.val() !== undefined && advanced_search_input.val().trim() === "") {
+        advanced_search_button.prop('disabled', true)
+    }
+
+    advanced_search_input.on('keyup paste', function() {
+        var input = $(this)
+        if (input.val().trim() === "") {
+            advanced_search_button.prop('disabled', true)
+        } else {
+            advanced_search_button.prop('disabled', false)
+        }
+    })
+
     adv_arrow.on('click', function() {
         var icon = $(this)
         if (icon.hasClass('fa-angle-up')) {
