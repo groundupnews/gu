27edb704c8f9e378e8403367f3fdefcd587b7eea
Nathan Geffen <nathangeffen@gmail.com>
2019-07-26 14:03:13 +0200

    Implemented variable number of top urls.

--- analyzer/top_urls.py
+++ analyzer/top_urls.py
@@ -19,7 +19,7 @@ import operator
 #     return lines
 
 
-def most_popular_pages(filename, cutoff_time):
+def most_popular_pages(filename, cutoff_time, num):
     with open(filename) as f:
         content = f.readlines()
     records = [CLFParser.logDict(l) for l in content]
@@ -37,7 +37,7 @@ def most_popular_pages(filename, cutoff_time):
             else:
                 urls[s] = 1
     most_pop = sorted(urls.items(), key=operator.itemgetter(1), reverse=True)
-    for url in most_pop[0:5]:
+    for url in most_pop[0:num]:
         print(url[0], url[1])
 
 
@@ -46,11 +46,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get most popular pages')
     parser.add_argument('--logfile', type=str, default="access.log")
     parser.add_argument('--date', type=str, default="")
+    parser.add_argument('--diff', type=int, default=10)
+    parser.add_argument('--num', type=int, default=5)
     options = parser.parse_args()
 
     if options.date == "":
-        cutoff_time = datetime.datetime.now() - datetime.timedelta(minutes=10)
+        cutoff_time = datetime.datetime.now() - \
+                      datetime.timedelta(minutes=options.diff)
     else:
         cutoff_time = datetime.datetime.strptime(options.date,
                                                  '%Y-%m-%d %H:%M:%S')
-    most_popular_pages(options.logfile, cutoff_time)
+    most_popular_pages(options.logfile, cutoff_time, options.num)
