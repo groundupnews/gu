6d62370a8735044b48b7114c2ec02927763fa396
Nathan Geffen <nathangeffen@gmail.com>
2019-09-28 23:53:54 +0200

    Republisher now supports sending embedded html recommendation for counting visits to third party sites.

--- republisher/models.py
+++ republisher/models.py
@@ -19,6 +19,8 @@ class Republisher(models.Model):
     email_addresses = models.CharField(max_length=250,
                                        validators=[validate_email_list, ])
     message = models.TextField(blank=True)
+    slug = models.SlugField(max_length=20, blank=True)
+    ask_for_analytics = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
