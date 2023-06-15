196e0b410ac5226141d7461d79d37514041c8334
WAbdoolKarim <w.abdoolkarim@gmail.com>
2023-06-15 02:41:21 +0200

    out of date initial commit. current build with api access issues still present

--- donationPage/views.py
+++ donationPage/views.py
@@ -6,6 +6,8 @@ from django.core.paginator import Paginator
 #for api request
 import requests
 import json
+import random
+from datetime import datetime
 
 from .models import Donor, Currency, Donation
 from .forms import DonationForm, CurrencyForm, DonorForm
@@ -52,16 +54,37 @@ def fetch_snapscan_transactions():
         return []
 
 
+def make_donorUrl():
+    min_val = 100000000000000000000000000000000000
+    max_val = 999999999999999999999999999999999999
+
+    # Get the current date and time as a numeric-only 14-digit string
+    current_date = datetime.now()
+    date_string = current_date.strftime("%Y%m%d%H%M%S")
+
+    # Generate a random 36-digit number
+    url = str(random.randint(min_val, max_val))
+
+    # Concatenate the random number and date
+    url += date_string
+
+    return url
+
 def save_transactions_to_database():
     transactions = fetch_snapscan_transactions()
     for donation in transactions:
-        reference = donation["reference"]
+        donor_email = donation["reference"]
         amount = donation["amount"]
         currency_type = donation["currency_type"]
         datetime_of_donation = donation["datetime_of_donation"]
         notified = False
         section18a = False
-        # Create a new Transaction object and save it to the database
+
+        Cdonor=Donor.objects.get(donor_url=donor_url)
+        if not Cdonor:
+            url=make_donorUrl()
+            newDonor = Donor(email=donor_email, name="anonymous", display_name="anonymous", donor_url=url)
+        # Create a new Donation object and save it to the database
         new_donation = Donation(donor=reference, amount=amount, datetime_of_donation=datetime_of_donation, currency_type=currency_type, notified=notified, section18a=section18a)
         new_donation.save()
 
