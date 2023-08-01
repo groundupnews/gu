4aa0c487707cae3606dfa634846c58f0f104cf65
WAbdoolKarim <w.abdoolkarim@gmail.com>
2023-08-01 12:08:22 +0200

    missing import

--- donationPage/management/commands/PullDonations.py
+++ donationPage/management/commands/PullDonations.py
@@ -3,6 +3,7 @@ import json
 import random
 import pytz
 import re
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from datetime import datetime, timezone, timedelta
