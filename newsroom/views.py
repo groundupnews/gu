c3c464da97afa50366491ce8ebb4027fa3d95985
Nathan Geffen <nathangeffen@gmail.com>
2024-12-27 19:32:31 +0200

    Holiday message.

--- newsroom/views.py
+++ newsroom/views.py
@@ -99,8 +99,8 @@ class HomePage(ArticleList):
 
     def get(self, request, *args, **kwargs):
         # Add messages here. E.g.
-        # messages.add_message(request, messages.INFO,
-        #                      "We are publishing irregularly, if at all, until 9 January 2024. Have a safe holiday season.")
+        messages.add_message(request, messages.INFO,
+                            "We are publishing irregularly, if at all, until 14 January 2025. Have a safe holiday season.")
         request = super(HomePage, self).get(request, args, kwargs)
         return request
 
