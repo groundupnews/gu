a0d84cee02f3a6ccbf33d24cfc9dee5025d3c464
Joel Cedras <joelcedras@gmail.com>
2025-01-21 11:08:12 +0200

    fixed q&a pagination -- search context now added to query

--- agony/views.py
+++ agony/views.py
@@ -34,19 +34,33 @@ class QandAList(ListView):
     def get_context_data(self, **kwargs):
         context = super(QandAList, self).get_context_data(**kwargs)
         context['qanda_search'] = True
-        if self.request.user.has_perm('agony.change_qanda'):
-            context['can_edit'] = True
-        else:
-            context['can_edit'] = False
+        
+        # query params dict for context -- needed for pagination
+        query_params = {}
         if 'topic' in self.request.GET:
+            query_params['topic'] = self.request.GET['topic']
             try:
                 topic = int(self.request.GET['topic'])
                 topic = Topic.objects.get(pk=topic)
                 context['topic'] = topic.name
             except:
                 pass
+
         if 'qanda_search_str' in self.request.GET:
+            query_params.update({
+                'qanda_search_str': self.request.GET['qanda_search_str'],
+                'search_type': self.request.GET.get('search_type', ''),
+                'is_simple': self.request.GET.get('is_simple', '')
+            })
             context['qanda_search_str'] = self.request.GET['qanda_search_str']
+
+        context['query_params'] = query_params
+        
+        if self.request.user.has_perm('agony.change_qanda'):
+            context['can_edit'] = True
+        else:
+            context['can_edit'] = False
+
         return context
 
 class QandADetail(DetailView):
