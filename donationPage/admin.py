969331a8091c8fe97753c0e5cf47183cf21cb1ac
Nathan Geffen <nathangeffen@gmail.com>
2025-11-03 13:14:08 +0200

    Improved donations admin.

--- donationPage/admin.py
+++ donationPage/admin.py
@@ -2,15 +2,42 @@ from django.contrib import admin
 
 from .models import Donor, Currency, Donation, Subscription
 
-#repeat for other models.
-class DonationAdmin(admin.ModelAdmin):
-    list_display = ('donor', 'datetime_of_donation',
-                    'currency_type', 'amount','notified','section18a_issued',)
-    #list_editable = ('donor', 'datetime_of_donation',
-     #               'currency_type', 'amount','notified','section18a_issued',)
-    
 
-admin.site.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "display_name", "email")
+    list_editable = ("name", "display_name", "email")
+    search_fields = ("name", "display_name", "email")
+
+
+# repeat for other models.
+class DonationAdmin(admin.ModelAdmin):
+    list_display = (
+        "donor",
+        "datetime_of_donation",
+        "currency_type",
+        "amount",
+        "notified",
+        "section18a_issued",
+    )
+    search_fields = [
+        "donor__name",
+        "donor__email",
+        "donor__pk",
+    ]
+    # list_editable = ('donor', 'datetime_of_donation',
+    #               'currency_type', 'amount','notified','section18a_issued',)
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("donor", "status", "amount")
+    search_fields = [
+        "donor__name",
+        "donor__email",
+        "donor__pk",
+    ]
+
+
+admin.site.register(Donor, DonorAdmin)
 admin.site.register(Currency)
-admin.site.register(Subscription)
+admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(Donation, DonationAdmin)
